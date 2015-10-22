f66b09d8bc6475545ee58d16e87f7cbdfcd7b6fd
southorange <octagonpentagonhexagon@gmail.com>
2015-10-22 19:40:21 -0400

    Update hi.py

--- d/hi.py
+++ d/hi.py
@@ -1,4 +1,11 @@
 print('Hi')
 w = raw_input
-raw_input('What is your name?')
-if len(w) >= 0:
+raw_input("What is your name?")
+if len(w) >= 0 and w.isalpha() = True:
+  print(w)
+elif len(w) <= 0 and w.isalpha() = False:
+  return "Type something without anynumbers please"
+else:
+  return "Type something!"
+#end
+  
